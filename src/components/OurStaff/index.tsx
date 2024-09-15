7eed1d6c669c7bfc8baf875f9802fd2992f7bf54
Ariel Campos <ariel.campos94@yahoo.com>
2024-09-15 12:26:07 -0300

    added OurStaff section

--- src/components/OurStaff/index.tsx
+++ src/components/OurStaff/index.tsx
@@ -1,9 +1,54 @@
-import { OurStaffContainer } from './styles'
+import {
+  OurStaffContainer,
+  Staff,
+  CardHeader,
+  StafCardfWithDescription,
+  CardImage,
+  CardDescription,
+  StaffCardSimple,
+} from "./styles";
+import mariaClaraGuide from "../../assets/images/maria-clara.jpg";
+import diegoGomesGuide from "../../assets/images/diego-gomes.jpg";
+import eduardoPassosGuide from "../../assets/images/eduardo-passos.jpg";
 
 export function OurStaff() {
-    return (
-        <OurStaffContainer className='container'>
-
-        </OurStaffContainer>
-    )
+  return (
+    <OurStaffContainer className="container">
+      <CardHeader>
+        <h4>Nossa equipe especializada</h4>
+        <p>
+          Com anos de experiência, nosso guia é hábil em fornecer comentários
+          perspicazes e tornar sua viagem interessante e envolvente. Da história
+          e cultura dos destinos que você visita aos costumes e tradições
+          locais, nosso guia lhe proporcionará uma riqueza de informações para
+          enriquecer suas viagens.
+        </p>
+      </CardHeader>
+      <Staff>
+        <StafCardfWithDescription>
+          <CardImage image={mariaClaraGuide} />
+          <CardDescription>
+            <h4>Maria Clara</h4>
+            <p>
+              <span>Idiomas:</span> Espanhol, inglês e português
+            </p>
+            <p>
+              <span>Experiência:</span> 5 anos
+            </p>
+            <p>
+              Buenas! Me chamo Maria Clara e serei sua guia. Se você estiver
+              procurando informações ou precisar de ajuda, estarei aqui para
+              ajudá-los.
+            </p>
+          </CardDescription>
+        </StafCardfWithDescription>
+        <StaffCardSimple image={diegoGomesGuide}>
+          <span>Diego Gomes</span>
+        </StaffCardSimple>
+        <StaffCardSimple image={eduardoPassosGuide}>
+          <span>Eduardo Passos</span>
+        </StaffCardSimple>
+      </Staff>
+    </OurStaffContainer>
+  );
 }
--- src/components/OurStaff/styles.ts
+++ src/components/OurStaff/styles.ts
@@ -1,4 +1,111 @@
 import styled from "styled-components";
 
+export const OurStaffContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 6rem;
 
-export const OurStaffContainer = styled.section``
+  margin-bottom: 6rem;
+  height: 100vh;
+  width: 100%;
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3.5rem;
+  align-self: flex-end;
+
+  text-align: justify;
+  width: 50%;
+
+  > h4 {
+    color: ${({ theme }) => theme.black};
+    font-family: Montserrat, sans-serif;
+    font-size: 2.5rem;
+    font-weight: 600;
+  }
+`;
+
+export const Staff = styled.div`
+  display: grid;
+  grid-template-columns: 45% 27.5% 27.5%;
+  column-gap: 2.5rem;
+  justify-content: space-between;
+
+  height: 39rem;
+  width: 100%;
+`;
+
+export const StafCardfWithDescription = styled.div`
+  display: flex;
+  align-items: center;
+
+  border-radius: 10px;
+  overflow: hidden;
+  height: 100%;
+`;
+
+interface CardImageProps {
+  image: string;
+}
+
+export const CardImage = styled.div<CardImageProps>`
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 50%;
+`;
+
+export const CardDescription = styled.div`
+  background-color: ${({ theme }) => theme["brown-200"]};
+  padding: 1.9rem 1rem;
+  height: 100%;
+  width: 50%;
+
+  > h4 {
+    color: ${({ theme }) => theme.white};
+    font-family: Montserrat, sans-serif;
+    font-size: 2rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+    text-transform: capitalize;
+  }
+
+  > p {
+    color: ${({ theme }) => theme.white};
+    font-family: Poppins, sans-serif;
+    font-size: 1.3rem;
+    margin-bottom: 1rem;
+    text-align: justify;
+
+    > span {
+      font-weight: 500;
+    }
+  }
+`;
+
+export const StaffCardSimple = styled(CardImage)`
+  border-radius: 10px;
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+  > span {
+    position: absolute;
+    left: 50%;
+    bottom: 2.5rem;
+    transform: translateX(-50%);
+
+    color: ${({ theme }) => theme.white};
+    font-family: Montserrat, sans-serif;
+    font-size: 2rem;
+    font-weight: 500;
+    text-align: center;
+    text-transform: capitalize;
+    white-space: nowrap;
+    z-index: 10;
+  }
+`;

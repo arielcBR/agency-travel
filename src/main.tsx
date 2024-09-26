502b850ceec8a0eb305cad4dd3e6cf6fb22a05c9
Ariel Campos <ariel.campos94@yahoo.com>
2024-09-26 19:44:29 -0300

    created method to verify if root id exists

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import { MenuProvider } from "./hooks/MenuContext.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MenuProvider>
-      <App />
-    </MenuProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <MenuProvider>
+        <App />
+      </MenuProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Elemento root não encontrado");
+}
